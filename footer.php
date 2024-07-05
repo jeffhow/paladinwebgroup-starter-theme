7a666f0922c37df1df77fc11730ceda431a1c0ed
Jeff <jeff@jeff.how>
2024-07-05 07:10:19 -0400

    Update footer.php template helper

--- footer.php
+++ footer.php
@@ -5,6 +5,12 @@
 <div id="copyright">
 &copy; <?php echo esc_html( date_i18n( __( 'Y', 'paladinwebgroup' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
 </div>
+
+<?php  // delete this Block in production
+    echo '<p style="background-color: red; color: yellow; font-size: 2rem; padding: 1rem;">';
+    global $template; echo basename($template); 
+    echo '</p>';
+?>
 </footer>
 </div>
 <?php wp_footer(); ?>
