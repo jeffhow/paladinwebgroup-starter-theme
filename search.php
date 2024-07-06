5a1830c6d4e69f4e2bae5e7bafc174008f825ded
Jeff LeBoeuf <jeff@jeff.how>
2024-07-06 19:16:37 -0400

    search and thumbnails

--- search.php
+++ search.php
@@ -1,21 +1,33 @@
 <?php get_header(); ?>
 <?php if ( have_posts() ) : ?>
 <header class="header">
-<h1 class="entry-title" itemprop="name"><?php printf( esc_html__( 'Search Results for: %s', 'paladinwebgroup' ), get_search_query() ); ?></h1>
+    <h1 class="entry-title" itemprop="name">
+        <?php printf( esc_html__( 'Search Results for: %s', 'paladinwebgroup' ), get_search_query() ); ?></h1>
 </header>
 <?php while ( have_posts() ) : the_post(); ?>
-<?php get_template_part( 'entry' ); ?>
+    <div class="search-result-item">
+
+        <?php get_template_part( 'entry' ); ?>
+        <div class="thumbnail-container">
+            <?php
+                if (has_post_thumbnail()) {
+                    the_post_thumbnail('thumbnail');
+                }
+            ?>
+        </div>
+    </div>
+
 <?php endwhile; ?>
 <?php get_template_part( 'nav', 'below' ); ?>
 <?php else : ?>
 <article id="post-0" class="post no-results not-found">
-<header class="header">
-<h1 class="entry-title" itemprop="name"><?php esc_html_e( 'Nothing Found', 'paladinwebgroup' ); ?></h1>
-</header>
-<div class="entry-content" itemprop="mainContentOfPage">
-<p><?php esc_html_e( 'Sorry, nothing matched your search. Please try again.', 'paladinwebgroup' ); ?></p>
-<?php get_search_form(); ?>
-</div>
+    <header class="header">
+        <h1 class="entry-title" itemprop="name"><?php esc_html_e( 'Nothing Found', 'paladinwebgroup' ); ?></h1>
+    </header>
+    <div class="entry-content" itemprop="mainContentOfPage">
+        <p><?php esc_html_e( 'Sorry, nothing matched your search. Please try again.', 'paladinwebgroup' ); ?></p>
+        <?php get_search_form(); ?>
+    </div>
 </article>
 <?php endif; ?>
 <?php get_footer(); ?>
